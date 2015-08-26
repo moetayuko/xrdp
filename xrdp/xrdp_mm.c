d6f0e835bda89cc102fd57b495943abb328b1f64
mirabilos <tg@mirbsd.org>
2015-08-26 21:04:21 +0200

    fix lots of warnings

--- xrdp/xrdp_mm.c
+++ xrdp/xrdp_mm.c
@@ -2274,7 +2274,7 @@ xrdp_mm_check_wait_objs(struct xrdp_mm *self)
 
 /*****************************************************************************/
 /* frame ack from client */
-int APP_CC
+static int APP_CC
 xrdp_mm_frame_ack(struct xrdp_mm *self, int frame_id)
 {
     int ex;
